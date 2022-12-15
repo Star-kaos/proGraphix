2b94f1263a27da32739da22aeab92308d60d72d5
Star_kaos <perera.elijah@gmail.com>
2022-12-15 15:19:22 -0800

    initial commit

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,25 @@
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="navbar ">
+
+            </div>
+            <div className="navbarLinks">
+                <ul>
+                    <li>
+
+                    </li>
+                    <li>
+                        q
+                    </li>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
